73f5e00b50427d4d066eef5aa02e0e129560937b
Giraudux <giraudux@gmail.com>
2018-03-01 20:22:13 +0100

    peniiiiis

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ class Data {
 }
 
 class Ride {
-    constructor(public i?: number, public a?: number, public b?: number, public x?: number, public y?: number, public s?: number, public f?: number) {}
+    constructor(public i: number, public a?: number, public b?: number, public x?: number, public y?: number, public s?: number, public f?: number) {}
 }
 
 class Solution {
@@ -30,10 +30,16 @@ class RideSolution {
     constructor(public M?: number, public R?: number[]) {}
 }
 
+class Car {
+    constructor(public i: number, public rideI: number = -1) {}
+}
+
 let filenames = ['a_example.in', 'b_should_be_easy.in', 'c_no_hurry.in', 'd_metropolis.in', 'e_high_bonus.in'];
 
 //forEach file in the folder in
 filenames.forEach(filename => {
+    let cars: Car[] = [];
+
     const data = new Data();
     let lines = fs.readFileSync('in/' + filename, 'utf-8').split('\n');
 
@@ -51,8 +57,7 @@ filenames.forEach(filename => {
     for (let i = 0; i < data.N; i++) {
         let line = lines[1 + i].split(' ');
 
-        let ride = new Ride();
-        ride.i = i;
+        let ride = new Ride(i);
         ride.a = +line[0];
         ride.b = +line[1];
         ride.f = +line[2];
@@ -70,6 +75,10 @@ filenames.forEach(filename => {
         solutionRide.M = 1;
         solutionRide.R = [i];
         solution.rides.push(solutionRide);
+    }
+
+    for(let i = 0; i < data.F; i++) {
+        cars[i] = new Car(i);
     }
 
     fs.writeFile('out/' + filename + '.out', convertSolutionToFile(solution), function(err) {
